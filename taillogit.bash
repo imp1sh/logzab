c24ae80d61ed568be69a803e3d2c13238bbd6fa9
root <root@srv001.gosix.net>
2016-02-29 00:05:35 +0100

    invoke UserParameter logzab with three parameters: 1st logfile, 2nd offsetfilename, 3rd grep expression

--- taillogit.bash
+++ taillogit.bash
@@ -1,4 +1,23 @@
 #!/bin/bash
-source config.bash
+# this is where the script itself resides. Also the config file should be there actually
 scriptdir="$(dirname "$0")"
+if [ -f $scriptdir/config.bash ]; then
+	source $scriptdir/config.bash
+	echo "inhaled config file $scriptdir/config.bash"
+else
+	echo "File config.bash not in working directory. Working directory is $scriptdir"
+fi
+
+# first parameter given in the userparameter command is the logfile in which we would like to search
+# second parameter is offset filename. this parameter might conflict, so we just check if it's already taken and if so, just don't do the userparameter but exit with !=0
+# third parameter is the expression 
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+	echo "the necessary parameters are not given. Please RTFM and retry."
+	exit 1
+else
+	logfilepath=$1
+	offsetfilename=$2
+	expression=$3
+	$(sudo "$logtailpath" -f "$logfilepath" -o "$scriptdir/$offsetspath/$offsetfilename") $(| $expression)
+fi
 
